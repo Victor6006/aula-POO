04574761f12293192062a192e5402a8959983b63
VASCo6006 <alexandre.v@escolar.ifrn.edu.br>
2024-09-12 16:33:10 -0300

    Create Conta.cs

---
+++ Conta.cs
@@ -0,0 +1,15 @@
+public class Conta
+{
+    public string nome;
+
+    public int senha;
+
+    public int conta;
+
+    public Conta(string nomedeusuario, int senhadaconta,int numerodaconta)
+    {
+        nome = nomedeusuario;
+        senha = senhadaconta;
+        conta = numerodaconta;
+    }
+}
